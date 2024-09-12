663a998d32feec857c059b6d642604c2ae6d6b64
namgigeom <gigeomnam@gmail.com>
2024-09-12 13:19:14 +0900

    Two changed commit

--- 2_GitHub 기초 기능 입문 1 실습자료 (1)/prime_number.py
+++ 2_GitHub 기초 기능 입문 1 실습자료 (1)/prime_number.py
@@ -1,6 +1,6 @@
-def is_prime_number(x):
-    for num in range(2, x):
-        if x % num == 0:
-            print("The number {num1} is not a prime number.".format(num1=x))
+def is_prime_number(y):
+    for num in range(4, y):
+        if y % num == 0:
+            print("The number {num1} is not a prime number.".format(num1=y))
             return
-    print("The number {num1} is a prime number.".format(num1=x))
+    print("The number {num1} is a prime number.".format(num1=y))
